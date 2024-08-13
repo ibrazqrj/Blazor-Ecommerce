ac9fc4eab1827aadcd9be7adb6543559ee8de086
Ibrahim Zeqiraj <Ibrahim.zeqiraj@eoscop.ch>
2024-08-13 15:10:09 +0200

    Added Categories and added the categories to the 3 products

--- BlazorEcommerceNEW/BlazorEcommerceNEW/Data/DataContext.cs
+++ BlazorEcommerceNEW/BlazorEcommerceNEW/Data/DataContext.cs
@@ -9,6 +9,27 @@
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "VIP",
+                    Url = "vip"
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Packs",
+                    Url = "packs"
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Currencies",
+                    Url = "currencies"
+                }
+            );
+
         modelBuilder.Entity<Product>().HasData(
             new Product
             {
@@ -16,7 +37,8 @@
                 Title = "Hobbu - VIP",
                 Description = "Geniesse Hobbu mit verschiedenen Vorteile.",
                 ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTntRWnAmtPso89KDj2fqwHBE_aHRbf8mmSqQ&s",
-                Price = 9.99m
+                Price = 9.99m,
+                CategoryId = 1
             },
             new Product
             {
@@ -24,7 +46,8 @@
                 Title = "Hobbu - Rare Pack",
                 Description = "Bis zu 10 Rares für das Hobbu Hotel (Frei wählbar!)",
                 ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYy9p1iZa44jfYqXC2NNBU1e4QbUtW5g9xeQ&s",
-                Price = 7.99m
+                Price = 7.99m,
+                CategoryId = 2
             },
             new Product
             {
@@ -32,11 +55,14 @@
                 Title = "Hobbu - Diamonds (250)",
                 Description = "Erhalte 250 Diamonds für das Hobbu Hotel!",
                 ImageUrl = "https://scontent-zrh1-1.xx.fbcdn.net/v/t39.30808-6/274978441_10158590467262963_5370902555679068282_n.png?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=vOYzWEKGu0kQ7kNvgFk_LTV&_nc_ht=scontent-zrh1-1.xx&oh=00_AYByiUjSlVZghuOJ4fOKuQ2kWmBxsqzil7uPqguD6lFW6A&oe=66BFCF27",
-                Price = 6.99m
+                Price = 6.99m,
+                CategoryId = 3
             });
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
     }
 
 }
