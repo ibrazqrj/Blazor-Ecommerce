c531175807dceb9ab85f5f80aa4fabfc329b37e5
Ibrahim Zeqiraj <Ibrahim.zeqiraj@eoscop.ch>
2024-08-20 13:44:15 +0200

    Added CartService on the Client

--- BlazorEcommerceNEW/BlazorEcommerceNEW.Client/Services/CartService/CartService.cs
+++ BlazorEcommerceNEW/BlazorEcommerceNEW.Client/Services/CartService/CartService.cs
@@ -1,6 +1,38 @@
-﻿namespace BlazorEcommerceNEW.Client.Services.CartService
+﻿using Blazored.LocalStorage;
+
+namespace BlazorEcommerceNEW.Client.Services.CartService
 {
     public class CartService : ICartService
     {
+        private readonly ILocalStorageService _localStorage;
+        public CartService(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public event Action OnChange;
+
+        public async Task AddToCart(CartItem cartItem)
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+            if (cart == null)
+            {
+                cart = new List<CartItem>();
+            }
+            cart.Add(cartItem);
+
+            await _localStorage.SetItemAsync("cart", cart);
+        }
+
+        public async Task<List<CartItem>> GetCartItems()
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+            if (cart == null)
+            {
+                cart = new List<CartItem>();
+            }
+
+            return cart;
+        }
     }
 }
